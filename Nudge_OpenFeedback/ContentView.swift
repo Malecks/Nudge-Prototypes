8de3113d1987fcb08635edb1b210587b3c19e355
Alex Mathers <Malecks@Alexs-MacBook-Pro.local>
2020-05-19 17:56:56 -0400

    Initial Commit

---
+++ Nudge_OpenFeedback/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nudge_OpenFeedback
+//
+//  Created by Alex Mathers on 2020-05-19.
+//  Copyright © 2020 Malecks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
